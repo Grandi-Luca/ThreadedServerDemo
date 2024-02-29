1183d30376e4e388174715684ede8e56fabacad1
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-02-29 18:33:40 +0100

    Added Server class to manage client connection

--- server_base.py
+++ server_base.py
@@ -5,105 +5,139 @@ import time
 
 from const import IP, PORT, ADDR, FORMAT, SIZE, DISCONNECT_MSG, INIT_MSG, SERVER_MAX_TIMEOUT, SERVER_TIMEOUT_WITHOUT_CLIENT, SERVER_INIT_MSG, SERVER_NOT_READY_MSG
 
-ending_connection = False
+class Server():
+    def __init__(self):
+        """
+        Initializes the server object.
 
-def handle_func(conn: socket.socket, addr: tuple, msg: str) -> None:
-    """
-    Handle the client connection and process the received message.
+        The server object is responsible for creating and managing the server socket.
+        It binds the socket to the specified address and starts listening for incoming connections.
+        The server is initially set to not initialized and has a timeout value set for clients.
 
-    Args:
-        conn (socket.socket): The client socket connection.
-        addr (tuple): The address of the client.
-        msg (str): The message received from the client.
+        Args:
+            None
 
-    Returns:
-        None
-    """
-    print(f"[{addr}] {msg}")
-
-    # Simulate some processing time
-    time.sleep(random.randint(1, 4))
+        Returns:
+            None
+        """
+        self.server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(ADDR)
+        self.server.listen()
+        self.server_init: bool = False
+        self.server.settimeout(SERVER_TIMEOUT_WITHOUT_CLIENT)
     
-    msg: str = f"Msg received: {msg}"
-    conn.send(msg.encode(FORMAT))
-    conn.close()
-    
+    def _init_server(self) -> None:
+        """
+        Initialize the server.
 
-def handle_client_separatelly(conn: socket.socket, addr: tuple, msg: str) -> None:
-    """
-    Handles the client's request in a separate thread.
+        Args:
+            None
 
-    Args:
-        server (Server): The server object.
-        conn (socket): The client's socket connection.
-        addr (tuple): The client's address.
-        msg (str): The client's message.
+        Returns:
+            None
+        """
+        while not self.server_init:
+            conn, addr = self.server.accept()
+            recv_msg: str = conn.recv(SIZE).decode(FORMAT)
 
-    Returns:
-        None
-    """
-    thread: threading.Thread = threading.Thread(target=handle_func, args=(conn, addr, msg))
-    thread.start()
-    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+            if recv_msg == INIT_MSG:
+                print(f"[{addr}] initialization {recv_msg}")
 
+                # Simulate some processing time
+                time.sleep(random.randint(3, 10))
+                self.server_init = True
+                conn.send(SERVER_INIT_MSG.encode(FORMAT))
+                break
+            
+            if not self.server_init:
+                conn.send(SERVER_NOT_READY_MSG.encode(FORMAT))
 
-def main() -> None:
-    """
-    Entry point of the server application.
-    """
-    print("[STARTING] Server is starting...")
-    server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
-    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+            # reset timeout
+            self.server.settimeout(SERVER_MAX_TIMEOUT)
+            conn.close()
 
-    server_init: bool = False
-    server.settimeout(SERVER_TIMEOUT_WITHOUT_CLIENT)
+    def _handle_client_separatelly(self, conn: socket.socket, addr: tuple, msg: str) -> None:
+        """
+        Handles the client's request in a separate thread.
 
-    # init server
-    while not server_init:
-        conn, addr = server.accept()
-        recv_msg: str = conn.recv(SIZE).decode(FORMAT)
+        Args:
+            conn (socket): The client's socket connection.
+            addr (tuple): The client's address.
+            msg (str): The client's message.
 
-        if recv_msg == INIT_MSG:
-            print(f"[{addr}] initialization {recv_msg}")
+        Returns:
+            None
+        """
+        thread: threading.Thread = threading.Thread(target=self._handle_func, args=(conn, addr, msg))
+        thread.start()
 
-            # Simulate some processing time
-            time.sleep(random.randint(3, 10))
-            server_init = True
-            conn.send(SERVER_INIT_MSG.encode(FORMAT))
-            break
+    def _handle_func(self, conn: socket.socket, addr: tuple, msg: str) -> None:
+        """
+        Handle the client connection and process the received message.
+
+        Args:
+            conn (socket.socket): The client socket connection.
+            addr (tuple): The address of the client.
+            msg (str): The message received from the client.
+
+        Returns:
+            None
+        """
+        print(f"[{addr}] {msg}")
+
+        # Simulate some processing time
+        time.sleep(random.randint(1, 4))
         
-        if not server_init:
-            conn.send(SERVER_NOT_READY_MSG.encode(FORMAT))
-
-        # reset timeout
-        server.settimeout(SERVER_MAX_TIMEOUT)
+        msg: str = f"Msg received: {msg}"
+        conn.send(msg.encode(FORMAT))
         conn.close()
 
-    
-    print("[SERVER INITIALIZED] Server is ready to receive requests")
+    def run(self) -> None:
+        """
+        Run the server.
 
-    # handle clients request
-    while True:
-        conn, addr = server.accept()
-        recv_msg: str = conn.recv(SIZE).decode(FORMAT)
+        Args:
+            None
 
-        if recv_msg == DISCONNECT_MSG:
-            print("[ENDING] Ending server connection")
-            break
+        Returns:
+            None
+        """
+        print("[STARTING] Server is starting...")
+        print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+        self._init_server()
+        print("[SERVER INITIALIZED] Server is ready to receive requests")
 
+        # handle clients request
+        while True:
+            conn, addr = self.server.accept()
+            recv_msg: str = conn.recv(SIZE).decode(FORMAT)
 
-        # handle client's request in a separate thread
-        handle_client_separatelly(conn, addr, recv_msg)
-        # reset timeout
-        server.settimeout(SERVER_MAX_TIMEOUT)
+            if recv_msg == DISCONNECT_MSG:
+                print("[ENDING] Ending server connection")
+                break
 
-    server.close()
+            # handle client's request in a separate thread
+            self._handle_client_separatelly(conn, addr, recv_msg)
+            # reset timeout
+            self.server.settimeout(SERVER_MAX_TIMEOUT)
+
+    def close(self) -> None:
+        """
+        Close the server connection.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.server.close()
 
 
 if __name__ == "__main__":
     try:
-        main()
+        server: Server = Server()
+        server.run()
+        server.close()
     except socket.timeout:
         pass
